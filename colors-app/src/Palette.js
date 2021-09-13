a908159be37350beb627523e38b44d08aa282322
Uppinder Chugh <uppinderchugh@gmail.com>
2021-09-13 09:18:51 +0530

    Style slider

--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -22,18 +22,20 @@ class Palette extends Component {
     const { level } = this.state;
 
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color.hex} name={color.name} />
+        <ColorBox background={color.hex} name={color.name} />
     ))
 
     return (
       <div className='Palette'>
-        <Slider 
-          defaultValue={level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={this.changeLevel}
-          />
+        <div className='slider'>
+            <Slider 
+            defaultValue={level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={this.changeLevel}
+            />
+        </div>
         {/* Navbar goes here */}
         <div className='Palette-colors'>
           {colorBoxes}
