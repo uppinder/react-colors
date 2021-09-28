e34f1ce7b88d06be585f5924df46b6a6b97050aa
Uppinder Chugh <uppinderchugh@gmail.com>
2021-09-28 18:50:56 +0530

    Save palettes to localStorage

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -10,11 +10,15 @@ import SingleColorPalette from "./SingleColorPalette";
 class App extends Component {
   constructor(props) {
     super(props);
+
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
     this.state = {
-      palettes: seedColors,
+      palettes: savedPalettes || seedColors,
     };
+
     this.findPalette = this.findPalette.bind(this);
     this.savePalette = this.savePalette.bind(this);
+    this.syncLocalStorage = this.syncLocalStorage.bind(this);
   }
 
   findPalette(paletteId) {
@@ -22,7 +26,17 @@ class App extends Component {
   }
 
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
+  }
+
+  syncLocalStorage() {
+    window.localStorage.setItem(
+      "palettes",
+      JSON.stringify(this.state.palettes)
+    );
   }
 
   render() {
--- colors-app/src/PaletteFormNav.js
+++ colors-app/src/PaletteFormNav.js
@@ -8,7 +8,6 @@ import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
 import PaletteSaveForm from "./PaletteSaveForm";
 
